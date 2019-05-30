0cd0a1f534537386b981ea6930f4ebfd8d773253
sts-msys <stsiwo@gmail.com>
2019-05-30 15:59:56 -0700

    updated oidc config file

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -2,10 +2,10 @@ import { createUserManager } from 'redux-oidc';
 
 const settings = {
   // the user manager settings for oidc-client
-  authority: "http://localhost:5000",
+  authority: "http://localhost:5010",
   client_id: "spa.react",
   redirect_uri: "http://localhost:8080/callback",
-  response_type: "password",
+  response_type: "code",
   silent_redirect_uri: "http://localhost:8080/silent-renew",
   automaticSilentRenew: true,
   scope:"openid",
